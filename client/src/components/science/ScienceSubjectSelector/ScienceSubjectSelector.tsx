78db7f01b27085dc183e335e0d53a1e86fb25a82
angelonicolson <angelonicolson@outlook.com>
2025-10-05 17:11:07 +1300

    Fixed broken CSS imports in science components

--- client/src/components/science/ScienceSubjectSelector/ScienceSubjectSelector.tsx
+++ client/src/components/science/ScienceSubjectSelector/ScienceSubjectSelector.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './MathGradeSelector.css'; // Reuse the same styles
+import '../../math/MathGradeSelector/MathGradeSelector.css'; // Reuse the same styles
 
 interface ScienceSubjectSelectorProps {
   onSubjectSelect: (subjectId: string) => void;
--- client/src/components/science/ScienceTutorCards/ScienceTutorCards.tsx
+++ client/src/components/science/ScienceTutorCards/ScienceTutorCards.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './TutorCards.css'; // Reuse the same styles
+import '../../tutoring/TutorCards/TutorCards.css'; // Reuse the same styles
 
 interface ScienceTutor {
   id: number;
